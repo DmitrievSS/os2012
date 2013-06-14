9f5e4ae94465d5410fadcbbbb1d574992a8a62aa
dmitrievss <seregas.dmitrias@gmail.com>
2013-06-14 20:50:37 +0400

    HMMMmmm

--- copy-paster/cp-epoll.c
+++ copy-paster/cp-epoll.c
@@ -10,29 +10,38 @@
 #include <stdlib.h>
 #include <sys/epoll.h>
 #include <signal.h>
+#include <sys/stat.h>
+#include <sys/sendfile.h>
+
 
 int EPOLL_RUN_TIMEOUT=-1;
 int main (int argc, char** argv){
-    struct epoll_event ev, events[argc-1];
+    struct epoll_event ev[argc-1], events[argc-1];
     int epoll_fd=epoll_create(argc-1);
     for (int i = 0; i <  (argc-1)/2; i ++){
-        ev.events = EPOLLIN;
-        ev.data.fd=atoi(argv[2*i+1]);
-        int res = epoll_ctl(epoll_fd, EPOLL_CTL_ADD,atoi(argv[2*i+1]), &ev);
+        ev[2*i].events = EPOLLIN;
+        int res = epoll_ctl(epoll_fd, EPOLL_CTL_ADD,atoi(argv[2*i+1]), &ev[2*i]);
+        ev[2*i+1].events = EPOLLOUT;
+        res = epoll_ctl(epoll_fd, EPOLL_CTL_ADD,atoi(argv[2*i+2]), &ev[2*i+1]);
     }
-    while (1) {
-        int ndfs = epoll_wait(epoll_fd, events,argc-1,EPOLL_RUN_TIMEOUT);
+    int n = (argc-1)/2;
+    while (n>0) {
+        int ndfs = epoll_wait(epoll_fd, events,argc-1,EPOLL_RUN_TIMEOUT+2);
+        if (ndfs!=0) printf("ndfs - %i\n", ndfs);
         if (ndfs < 0){
             printf("error %i\n", errno);
             break;
         }
         for (int i = 0; i < ndfs/2; i++){
             if (events[2*i].events==EPOLLIN && events[2*i+1].events==EPOLLOUT){
+                struct stat* buffer = (struct stat*)malloc(sizeof(struct stat));
+                fstat(events[2*i].data.fd, buffer);
+                sendfile(events[2*i+1].data.fd,events[2*i].data.fd,0,buffer->st_size);   
                 events[2*i].events = 0;
                 events[2*i+1].events = 0;
+                n--;
             }
         } 
     }
     return 0;
-
 }
